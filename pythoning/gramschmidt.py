031f6dd2af32b53170946c51854905092460e52a
nope <michaalbert.42@gmail.com>
2016-09-03 16:28:26 +0200

    Gram-Schmidt process using the default scalar product

--- pythoning/gramschmidt.py
+++ pythoning/gramschmidt.py
@@ -1,6 +1,8 @@
 import math
+#vectors are implemented as lists here
 
 def default_scalar_prod(vector1, vector2):
+	"""Calculates the default scalar product of two vectors."""
 	res = 0
 	v = zip(vector1, vector2)
 	for i in v:
@@ -8,27 +10,20 @@ def default_scalar_prod(vector1, vector2):
 	return res
 	
 def norm(vector):
+	"""Calculates the Euclidean norm of a vector."""
 	return math.sqrt(default_scalar_prod(vector, vector))
 
 def gramschmidt(vectors):
-	#res: liste von listen(vektoren), erstes element wird schon gegeben
+	"""Orthonormalizes a set of vectors using the Gram-Schmidt-process."""
 	res = [map(lambda x: x*(1/norm(vectors[0])), vectors[0])]
 	del vectors[0]
-	#zws: liste mit zwischenergebnissen: <v_k, v'_i>v'_i
 	zws = []
 	
 	for v in vectors:
 		for w in res:
-			zws.append(map(lambda x: x*default_scalar_prod(v, w), w))
-			
-		#erzeugt v_k" = v_k - sum_i=1 ^k-1(<v_k, v'_i>v'_i)	
+			zws.append(map(lambda x: x*default_scalar_prod(v, w), w))	
 		for	z in zws:
 			v = map(lambda x: x[0]-x[1], zip(v, z))
-			
-		#v_k' = 1/norm(v_k")*v_k"
 		v = map(lambda x: x*(1/norm(v)), v)
-		#v_k' res anfuegen
 		res.append(v)
 	return res
-	
-
